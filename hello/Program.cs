4896308ef2fb18dbd58a28eca7ea9f10a5fc3d3a
gaikwad121 <jg.jyotigaikwad121@gmail.com>
2025-03-16 20:53:32 +0530

    Add project files.

---
+++ hello/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+    public class hell
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+
+            Console.ReadLine();
+        }
+    }
+
+    /*int[] Number = { 9, 8, 1, 2, 6, 3, 4, 5, 7 };
+    Console.WriteLine(Number.Max());
+    Console.WriteLine(Number.Min());
+    Console.WriteLine(Number.Sum());*/
+    // 2D array
+    //int[,] num = { { 1, 2, 3 }, { 4, 5, 6 } };
+    /*  foreach(int num1 in num){
+          Console.WriteLine(num1);
+
+    int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.WriteLine(numbers[i, j]);
+        }
+    }
+    Console.ReadLine();
+    }
+    }
+    }
+    /* Array.Sort(Number);
+    foreach(int no in Number)
+    {
+    Console.WriteLine(no);
+    }
+    Console.ReadLine();
+    }
+    }
+    }
+
+    /* Arrays in string sortC#
+    string[] Name = { "jyoti", "Dharati", "Nanda", "Aishwarya", "Sneha", "Aaradhya", "Swara" };
+    Array.Sort(Name);
+    foreach(string list in Name)
+    {
+    Console.WriteLine(list);
+    }
+    Console.ReadLine();
+    } }
+    }
+
+
+    /*Name[0] = "Jyoti";
+    for (int i = 0; i < Name.Length; i++)
+    Console.WriteLine(Name[i]);
+    Console.WriteLine(Name.Length);
+    // foreach (string li in Name)
+    //  Console.WriteLine(li);
+    Console.WriteLine(Name[5]);
+    int[] arr = { 1, 2, 3, 4, 5, 6 };
+    Console.WriteLine(arr[2]);
+    Console.WriteLine(arr.Length);
+    }
+    }
+    }
+    /*use a while loop
+    int num = 10;
+    int i = 0;
+
+    while (i <= num) // Loop until i is 10
+    {
+    if (i == 4)
+    {
+    // Skip printing when i is 4, but increment i
+    i++;
+    continue;
+    }
+    Console.WriteLine(i);
+    i++; // Increment i normally
+
+
+    /* For each loop // break also used // continue also used
+    string[] name = { "mango", "Banana", "Apple", "Tomato", "Cucumber" };
+    foreach (string list in name)
+    {
+    if (list.Equals("Banana"))
+    {
+     continue;
+    }
+    Console.WriteLine(list);
+    }
+    Console.ReadLine();
+    }
+    /*  if (list.Equals("Apple"))
+    {
+     break;
+    }
+    Console.Write(list+" ");
+    }
+    /*int a = 15;
+    //Console.WriteLine(a);
+    Console.WriteLine("Hello world");
+    Console.Write("I love jyoti");
+    Console.ReadLine();
+
+    }
+    }
+    }
+    }*/
+
+
